b691e5a3eac43f9fd6bcdab542937984c8328494
iamsaad2 <saadbadat.1@gmail.com>
2020-07-15 23:04:49 -0400

    details

--- models/Flower.js
+++ models/Flower.js
@@ -9,7 +9,15 @@ const FlowerSchema = mongoose.Schema({
     type: String,
   },
 
+  winfo: {
+    type: String,
+  },
+
   earth: {
+    type: String,
+  },
+
+  einfo: {
     type: String,
   },
 
@@ -17,17 +25,38 @@ const FlowerSchema = mongoose.Schema({
     type: String,
   },
 
+  einfo2: {
+    type: String,
+  },
+
   air: {
     type: String,
   },
+
+  ainfo: {
+    type: String,
+  },
+
   air2: {
+    type: String,
+  },
+
+  ainfo2: {
     type: String,
   },
 
   air3: {
     type: String,
   },
+
+  ainfo3: {
+    type: String,
+  },
   air4: {
+    type: String,
+  },
+
+  ainfo4: {
     type: String,
   },
 
@@ -35,25 +64,59 @@ const FlowerSchema = mongoose.Schema({
     type: String,
   },
 
+  finfo: {
+    type: String,
+  },
+
   fire2: {
     type: String,
   },
+
+  finfo2: {
+    type: String,
+  },
+
   fire3: {
     type: String,
   },
+
+  finfo3: {
+    type: String,
+  },
+
   fire4: {
     type: String,
   },
+
+  finfo4: {
+    type: String,
+  },
+
   fire5: {
     type: String,
   },
+
+  finfo5: {
+    type: String,
+  },
+
   fire6: {
+    type: String,
+  },
+
+  finfo6: {
     type: String,
   },
   fire7: {
     type: String,
   },
+  finfo7: {
+    type: String,
+  },
   fire8: {
+    type: String,
+  },
+  finfo8: {
     type: String,
   },
 
--- routes/flowers.js
+++ routes/flowers.js
@@ -30,6 +30,21 @@ router.post(
       fire7,
       fire8,
       date,
+      winfo,
+      einfo,
+      einfo2,
+      ainfo,
+      ainfo2,
+      ainfo3,
+      ainfo4,
+      finfo,
+      finfo2,
+      finfo3,
+      finfo4,
+      finfo5,
+      finfo6,
+      finfo7,
+      finfo8,
     } = req.body;
 
     try {
@@ -51,6 +66,21 @@ router.post(
         fire7,
         fire8,
         date,
+        winfo,
+        einfo,
+        einfo2,
+        ainfo,
+        ainfo2,
+        ainfo3,
+        ainfo4,
+        finfo,
+        finfo2,
+        finfo3,
+        finfo4,
+        finfo5,
+        finfo6,
+        finfo7,
+        finfo8,
       });
 
       await flower.save();
@@ -111,6 +141,21 @@ router.put('/:id', async (req, res) => {
     fire7,
     fire8,
     date,
+    winfo,
+    einfo,
+    einfo2,
+    ainfo,
+    ainfo2,
+    ainfo3,
+    ainfo4,
+    finfo,
+    finfo2,
+    finfo3,
+    finfo4,
+    finfo5,
+    finfo6,
+    finfo7,
+    finfo8,
   } = req.body;
   let fObj = {};
   if (name) fObj.name = name;
@@ -130,6 +175,21 @@ router.put('/:id', async (req, res) => {
   if (fire7) fObj.fire7 = fire7;
   if (fire8) fObj.fire8 = fire8;
   if (date) fObj.date = date;
+  if (winfo) fObj.winfo = winfo;
+  if (einfo) fObj.einfo = einfo;
+  if (einfo2) fObj.einfo2 = einfo2;
+  if (ainfo) fObj.ainfo = ainfo;
+  if (ainfo2) fObj.ainfo2 = ainfo2;
+  if (ainfo3) fObj.ainfo3 = ainfo3;
+  if (ainfo4) fObj.ainfo4 = ainfo4;
+  if (finfo) fObj.finfo = finfo;
+  if (finfo2) fObj.finfo2 = finfo2;
+  if (finfo3) fObj.finfo3 = finfo3;
+  if (finfo4) fObj.finfo4 = finfo4;
+  if (finfo5) fObj.finfo5 = finfo5;
+  if (finfo6) fObj.finfo6 = finfo6;
+  if (finfo7) fObj.finfo7 = finfo7;
+  if (finfo8) fObj.finfo8 = finfo8;
 
   try {
     const updatedFlower = await Flower.findByIdAndUpdate(
